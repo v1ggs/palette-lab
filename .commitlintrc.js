63cd213845d7bf2cf1bfbe79dff739a004b76e11
v1ggs <90190553+v1ggs@users.noreply.github.com>
2024-03-13 16:34:12 +0100

    chore: fix commit linting

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
    // https://github.com/conventional-changelog/commitlint#shared-configuration
    extends: ['@commitlint/config-conventional'],
+   ignores: [message => message.includes('Initial commit')],
 };
